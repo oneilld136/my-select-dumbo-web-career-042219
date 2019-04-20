9fa02e3359aeaacd9abff828d60a377a2717ad75
oneilld136 <oneilld136@gmail.com>
2019-04-20 20:25:49 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
-end
+  count = 0
+  result = []
+  while count < array.size
+    if yield(array[count]) == true
+    result.push(array[count])
+  end
+    count+=1
+  end
+  result
+  end
